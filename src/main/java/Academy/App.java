b50db8ca72c6da605d88932cbcc1317a0a2a7b39
gaurav <seleniumdummyuser@gmail.com>
2021-08-08 18:00:02 +0530

    second commit

--- src/main/java/Academy/App.java
+++ src/main/java/Academy/App.java
@@ -9,5 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        
+        System.out.println("Checking E2E project the git status");
     }
 }
